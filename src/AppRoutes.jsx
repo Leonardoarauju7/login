303a031e8551a93547f87fa64cd5b45e663ad8aa
Leonardoarauju7 <leonardoearaujo@gmail.com>
2022-09-16 11:23:17 -0300

    Autenticação de rotas private.

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,22 +1,39 @@
 import {
   BrowserRouter as Router,
+  Navigate,
   Route,
   Routes,
   //navigate,
 } from "react-router-dom"
 
-import React, { useState } from "react"
+import React, { useContext } from "react"
 import LoginPage from "./Pages/LoginPage"
 import HomePage from "./Pages/HomePage"
-import { AuthProvider } from "./Context/AuthContext"
+import { AuthProvider, AuthContext } from "./Context/AuthContext"
 
 const AppRoutes = () => {
+  const Private = ({ children }) => {
+    const { authenticated } = useContext(AuthContext)
+
+    if (!authenticated) {
+      return <Navigate to="/login" />
+    }
+    return children
+  }
   return (
     <Router>
       <AuthProvider>
         <Routes>
           <Route exact path="/login" element={<LoginPage />} />
-          <Route exact path="/" element={<HomePage />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <Private>
+                <HomePage />
+              </Private>
+            }
+          />
         </Routes>
       </AuthProvider>
     </Router>
