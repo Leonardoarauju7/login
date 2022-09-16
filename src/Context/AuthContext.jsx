04c637906b5d89f503abf8e682b1a729b6767a79
Leonardoarauju7 <leonardoearaujo@gmail.com>
2022-09-16 13:30:41 -0300

    integração api. falta osdados na api pra funcionar

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, createContext } from "react"
 import { useNavigate } from "react-router-dom"
 
+import { api, createSession } from "../services/api"
+
 export const AuthContext = createContext()
 export const AuthProvider = ({ children }) => {
   const navigate = useNavigate()
@@ -9,6 +11,7 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const recuperarUser = localStorage.getItem("user")
+    const token = localStorage.getItem("token")
     if (recuperarUser) {
       setUser(JSON.parse(recuperarUser))
     }
@@ -16,23 +19,30 @@ export const AuthProvider = ({ children }) => {
     setLoading(false) //para de carregar o loading depois que é finalizado o carregamento.
   }, [])
 
-  const login = (email, password) => {
-    console.log("login auth", { email, password })
+  const login = async (email, password) => {
+    const response = await createSession(email, password)
+    console.log("login", response.data)
+    const loggedUser = response.data.user
+    const token = response.data.token
 
-    //api criar uma session
+    //armazenar em local
+    //const loggedUser = { id: "123", email } //moock pra simular os dados da api no lugar da linha 36
 
-    const loggedUser = { id: "123", email } //moock pra simular os dados da api
     localStorage.setItem("user", JSON.stringify(loggedUser)) // guardando no local storage
+    localStorage.setItem("token", token)
 
-    if (password === "secret") {
-      setUser({ loggedUser })
-      navigate("/")
-    }
+    api.defaults.headers.Authorization = `Bearer ${token}` //seta a api e faz com que ela envie sempre o bearer token quando é feita uma requisição.
+
+    //if (password === "secret") {}  não preciso mais desse if pq a api ja ta setando e fazendo a validação.
+    setUser({ loggedUser })
+    navigate("/")
   }
 
   const logout = () => {
     console.log("logout")
     localStorage.removeItem("user")
+    localStorage.removeItem("token")
+    api.defaults.headers.Authorization = null
     setUser(null)
     navigate("/login")
   }
--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,19 +1,40 @@
 import React from "react"
-import { useContext } from "react"
+import { useEffect, useContext, useState } from "react"
 import { AuthContext } from "../../Context/AuthContext"
+import { getUsers } from "../../services/api"
 
 const HomePage = () => {
-  const { logout, authenticated } = useContext(AuthContext) // recupero a funcão logout e chamo ela em handleLogout linha 7.
+  const { logout } = useContext(AuthContext) // recupero a funcão logout e chamo ela em handleLogout linha 7.
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await getUsers()
+      setUsers(response.data)
+      setLoading(false)
+    })()
+  }, [])
 
   const handleLogout = () => {
     logout()
   }
 
+  if (loading) {
+    return <div className="loading"> Carregando dados...</div>
+  }
+
   return (
     <>
       <h1>HomePage</h1>
-      <p>{String(authenticated)}</p>
       <button onClick={handleLogout}>Logout</button>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>
+            {user._id} - {user.email}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
