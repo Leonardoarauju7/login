137388f82b0a2ae7ea02f3c445d37b77c0fa049f
Leonardoarauju7 <leonardoearaujo@gmail.com>
2022-09-27 17:28:27 -0300

    HomePage

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -13,21 +13,54 @@ const HomePage = () => {
   return (
     <>
       <div className="nav-bar">
-        <ul>
+        <ul class="topnav">
           <li>
-            <a>NeoEnergia |</a>
+            <a class="active" href="#">
+              NEOENERGIA
+            </a>
           </li>
           <li>
-            <a>Home |</a>
+            <a href="#Home">Home</a>
           </li>
           <li>
-            <a>Lista de registros |</a>
+            <a href="#Lista de servicos">Lista de Serviços</a>
+          </li>
+          <li class="right">
+            <a href="#Logout" onClick={handleLogout}>
+              Sair
+            </a>
+
+            <p>{authenticated}</p>
           </li>
         </ul>
       </div>
-      <button onClick={handleLogout}>Sair</button>
-
-      <p>{authenticated}</p>
+      <div>
+        <div className="container-inputs">
+          <select className="select">
+            <option selected value="regiao">
+              Regiao
+            </option>
+            <option value="brasilia">Brasilia</option>
+          </select>
+          <input
+            className="endereco"
+            type="text"
+            name="endereço"
+            placeholder="Endereço"
+          />
+          <select className="select">
+            <option selected value="agente">
+              Agente
+            </option>
+          </select>
+          <button className="btnhome">Incluir carga</button>
+          <button className="btnhome1">Atribuir</button>
+        </div>
+        <div className="container-geo"></div>
+        <div className="periodo-referente">
+          <p>Período referente a</p>
+        </div>
+      </div>
     </>
   )
 }
